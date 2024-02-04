34663c196ff59810e7ec568804fadd227ebde21b
Bince k c <62138187+bincekc@users.noreply.github.com>
2024-02-04 17:15:33 +0530

    Commit_1
    folder structure in api changed
    teacher logics addeed

---
+++ API/nms-backend-api/Entity/Teacher.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace nms_backend_api.Entity
+{
+    [Table("tbl_teacher")]
+    public class Teacher
+    {
+        [Key]
+        public int TeacherId { get; set; }
+
+        [Required] //set not null constraint
+        [Column("FirstName", TypeName = "varchar")]
+        public string FName { get; set; }
+
+        [Required] //set not null constraint
+        [Column("LastName", TypeName = "varchar")]
+        public string LName { get; set; }
+        [Required] //set not null constraint
+        [Column("Class", TypeName = "varchar")]
+        [StringLength(30)]
+        public string Class { get; set; }//to be enum
+
+        public DateTime DOB { get; set; }
+
+        [Column(TypeName = "char")]
+        [StringLength(5)]
+        public string Gender { get; set; }//to be enum
+
+
+        [Column(TypeName = "varchar")]
+        [StringLength(30)]
+        public string Subject { get; set; }
+    }
+}
---
+++ API/nms-backend-api/Logics/Concrete/TeacherRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using nms_backend_api.Entity;
+using nms_backend_api.Logics.Contract;
+
+namespace nms_backend_api.Logics.Concrete
+{
+    public class TeacherRepository : ITeacherRepository
+    {
+        private readonly TContext _context;
+
+        public TeacherRepository(TContext context)
+        {
+            _context = context;
+        }
+        public void Add(Teacher teacher)
+        {
+            _context.teachers.Add(teacher);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            Teacher teacher = _context.teachers.Find(id);
+            _context.teachers.Remove(teacher);
+            _context.SaveChanges();
+        }
+
+        public List<Teacher> GetAll()
+        {
+           return _context.teachers.ToList();
+        }
+
+        public Teacher GetTeacher(int id)
+        {
+            return _context.teachers.Find(id);
+        }
+
+        public List<Teacher> GetTeachersByClass(string class1)
+        {
+            List<Teacher> teachersByclass = _context.teachers.Where((e) => e.Equals(class1)).ToList();
+            return teachersByclass;
+        }
+
+        public List<Teacher> GetTeachersBySubject(string subject)
+        {
+            List<Teacher> teachersbysub=_context.teachers.Where((e)=>e.Equals(subject)).ToList();
+            return teachersbysub;
+        }
+
+        public void Update(Teacher teacher)
+        {
+            
+        }
+    }
+}
