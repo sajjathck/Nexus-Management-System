fea77354254dde836c4b82e24d907ff10143e70d
abiya-varghese <abiyaelsavarghese23@gmail.com>
2024-02-05 14:46:04 +0530

    commit-5

---
+++ API/nms-backend-api/Entity/Class.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace nms_backend_api.Entity
+{
+    [Table("tbl_class")]
+
+    public class Class
+    {
+       
+            [Key]
+            public int ClassId { get; set; }
+
+             [Required]
+             [StringLength(50)]
+             [Column("ClassName", TypeName = "varchar")]
+            public string ClassName { get; set; }
+
+            [ForeignKey("TeacherId")]
+            public Teacher? Teacher { get; set; }
+
+
+
+    }
+}
---
+++ API/nms-backend-api/Logics/Concrete/ClassRepository.cs
@@ -0,0 +1,73 @@
+﻿using nms_backend_api.Entity;
+using nms_backend_api.Logics.Contract;
+
+namespace nms_backend_api.Logics.Concrete
+{
+    public class ClassRepository : IClassRepository
+    {
+        public void Create(Class clas)
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<Class> GetAll()
+        {
+            try
+            {
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public Class GetClass(int id)
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void Update(Class clas)
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
